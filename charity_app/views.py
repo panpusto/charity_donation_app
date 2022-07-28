5fff934fe5d1f7fab6f0fc3ae9843a5a63e0a1a1
Cezary <102999499+panpusto@users.noreply.github.com>
2022-07-28 19:38:35 +0200

    add base.html and edited htmls

--- charity_app/views.py
+++ charity_app/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LandingPageView(View):
+    """This function display application main page."""
+    def get(self, request):
+        return render(
+            request,
+            'index.html'
+        )
+
+
+class AddDonationView(View):
+    """This function display form to add donation"""
+    def get(self, request):
+        return render(
+            request,
+            'form.html'
+        )
+
+
+class LoginView(View):
+    """This function display login form."""
+    def get(self, request):
+        return render(
+            request,
+            'login.html'
+        )
+
+
+class RegisterView(View):
+    """This function display register form for new user."""
+    def get(self, request):
+        return render(
+            request,
+            'register.html'
+        )
+
--- charity_donation_project/urls.py
+++ charity_donation_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from charity_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.LandingPageView.as_view(), name='landing-page'),
+    path('add-donation/', views.AddDonationView.as_view(), name='add-donation'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
