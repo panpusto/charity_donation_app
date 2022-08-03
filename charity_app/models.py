05f92250afddf03a50833bddd371c47b6d928515
Cezary <102999499+panpusto@users.noreply.github.com>
2022-08-03 23:35:11 +0200

    add dynamic institution loading from db on main page

--- charity_app/models.py
+++ charity_app/models.py
@@ -11,6 +11,9 @@ INSTITUTION_TYPE = {
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 
 class Institution(models.Model):
     name = models.CharField(max_length=60)
--- charity_app/views.py
+++ charity_app/views.py
@@ -1,8 +1,9 @@
+import random
 from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
 
-from charity_app.models import Donation
+from charity_app.models import Donation, Institution
 
 
 class LandingPageView(View):
@@ -12,12 +13,25 @@ class LandingPageView(View):
         bags_counter = bags_quantity['quantity__sum']
         institutions = Donation.objects.aggregate(Sum('institution'))
         institution_counter = institutions['institution__sum']
+
+        foundations = list(Institution.objects.filter(type=1))
+        random_foundations = random.sample(foundations, 3)
+
+        non_gov_organisations = list(Institution.objects.filter(type=2))
+        random_non_gov_org = random.sample(non_gov_organisations, 4)
+
+        fundraisers = list(Institution.objects.filter(type=3))
+        random_fundraisers = random.sample(fundraisers, 2)
+
         return render(
             request,
             'index.html',
             context={
                 'bags_counter': bags_counter,
                 'institution_counter': institution_counter,
+                'random_foundations': random_foundations,
+                'random_non_gov_org': random_non_gov_org,
+                'random_fundraisers': random_fundraisers,
             }
         )
 
